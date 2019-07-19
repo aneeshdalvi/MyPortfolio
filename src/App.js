4487547815498c6a38366edfff21d143a609e91a
aneeshdalvi <adalvi1@asu.edu>
2019-07-19 01:25:05 -0700

    Adding new react components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Maincontent from './Maincontent';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Maincontent />
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Maincontent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Maincontent extends React.components{
+    state : {
+
+    }
+
+    render(){
+        return (
+          <div>
+            <h1>This is a Maincontent component</h1>
+        </div>
+        )
+    }
+}
+
+export default Maincontent;
